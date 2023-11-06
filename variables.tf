556e336ed63ac0df1ccb516633a893734a178865
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-06 09:38:10 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "cidr_block"{
+}
+
+variable "private_subnet_cidr"{
+}
+
+variable "public_subnet_cidr_1"{
+}
+
+variable "public_subnet_cidr_2"{
+}
+
+variable "private_az"{
+}
+
+variable "public_az1"{
+}
+
+variable "public_az2"{
+}
