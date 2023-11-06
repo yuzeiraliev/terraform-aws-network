fe1382b05a08cea81e051816a609b71c7d4c2083
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-06 09:37:38 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,92 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = "Custom VPC"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.private_az
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_1
+  availability_zone       = var.public_az1
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "public_1b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_cidr_2
+  availability_zone       = var.public_az2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.private.id
+
+  tags = {
+    Name = "NAT"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "Private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1b" {
+  subnet_id      = aws_subnet.public_1b.id
+  route_table_id = aws_route_table.public.id
+}
