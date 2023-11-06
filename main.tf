b088fdf9fcdb9b1b9c8afed5be77c808fd14392a
yuzeiraliev <141018040+yuzeiraliev@users.noreply.github.com>
2023-11-06 14:41:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
   tags = {
     Name = "Private"
